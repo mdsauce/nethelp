d06635fb5f93f7ab801a1c9b181029b4453ef30f
mdsauce <max.dobeck@saucelabs.com>
2019-02-06 18:03:51 -0800

    updated build script so everyone gets a file called nethelp.

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,14 @@
-env GOOS=windows GOARCH=amd64 go build -o nethelp-windows
-env GOOS=linux GOARCH=amd64 go build -o nethelp-linux
-go build -o nethelp-mac
+env GOOS=windows GOARCH=amd64 go build -o nethelp
+mkdir builds/nethelp-windows
+mv nethelp builds/nethelp-windows
+cp README.md builds/nethelp-windows
 
-mv nethelp* builds
+env GOOS=linux GOARCH=amd64 go build -o nethelp
+mkdir builds/nethelp-linux
+mv nethelp builds/nethelp-linux
+cp README.md builds/nethelp-linux
+
+go build -o nethelp
+mkdir builds/nethelp-mac
+mv nethelp builds/nethelp-mac
+cp README.md builds/nethelp-mac

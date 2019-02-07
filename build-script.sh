63de9006c7eba6bd89086e7548ecb201d722000a
mdsauce <max.dobeck@saucelabs.com>
2019-02-06 18:27:40 -0800

    Added sanity check to make sure everything builds and installs.

--- build-script.sh
+++ build-script.sh
@@ -1,18 +1,20 @@
+go install
+
 env GOOS=windows GOARCH=amd64 go build -o nethelp
-mkdir builds/nethelp-windows
+mkdir -p builds/nethelp-windows
 mv nethelp builds/nethelp-windows
 cp README.md builds/nethelp-windows
 tar -C /Users/maxdobeck/go/src/github.com/mdsauce/nethelp/builds/ -czf nethelp-windows.tar.gz nethelp-windows
 
 env GOOS=linux GOARCH=amd64 go build -o nethelp
-mkdir builds/nethelp-linux
+mkdir -p builds/nethelp-linux
 mv nethelp builds/nethelp-linux
 cp README.md builds/nethelp-linux
 tar -C /Users/maxdobeck/go/src/github.com/mdsauce/nethelp/builds/ -czf nethelp-linux.tar.gz nethelp-linux
 
 
 go build -o nethelp
-mkdir builds/nethelp-mac
+mkdir -p builds/nethelp-mac
 mv nethelp builds/nethelp-mac
 cp README.md builds/nethelp-mac
 tar -C /Users/maxdobeck/go/src/github.com/mdsauce/nethelp/builds/ -czf nethelp-mac.tar.gz nethelp-mac

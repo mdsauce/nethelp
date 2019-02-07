6649df26c4b1586ca878a681e0ec2230f33125e2
mdsauce <max.dobeck@saucelabs.com>
2019-02-06 18:15:31 -0800

    Doing the tarfile method.

--- build-script.sh
+++ build-script.sh
@@ -2,13 +2,19 @@ env GOOS=windows GOARCH=amd64 go build -o nethelp
 mkdir builds/nethelp-windows
 mv nethelp builds/nethelp-windows
 cp README.md builds/nethelp-windows
+tar -czf nethelp-windows.tar.gz builds/nethelp-windows
 
 env GOOS=linux GOARCH=amd64 go build -o nethelp
 mkdir builds/nethelp-linux
 mv nethelp builds/nethelp-linux
 cp README.md builds/nethelp-linux
+tar -czf nethelp-linux.tar.gz builds/nethelp-linux
+
 
 go build -o nethelp
 mkdir builds/nethelp-mac
 mv nethelp builds/nethelp-mac
 cp README.md builds/nethelp-mac
+tar -czf nethelp-mac.tar.gz builds/nethelp-mac
+
+mv *.tar.gz builds

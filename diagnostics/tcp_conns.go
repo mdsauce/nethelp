7a5c414dbc62f115591cc4ecd7883664cb4666ee
mdsauce <max.dobeck@saucelabs.com>
2019-03-27 17:38:28 -0700

    Updated and split out diagnostics to be clearer as a whole package.

---
+++ diagnostics/tcp_conns.go
@@ -0,0 +1,51 @@
+package diagnostics
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/proxy"
+)
+
+// TCPConns attempts to open various TCP connections to the provided sites
+// This proves that with or without a proxy the TCP connections can be created.
+func TCPConns(sitelist []string, proxyURL *url.URL) {
+	if proxyURL != nil {
+		log.Warn("May fail if you are not using a SOCKS5 Proxy.")
+		var err error
+		var proxyDialer proxy.Dialer
+		proxyDialer, err = proxy.FromURL(proxyURL, proxy.Direct)
+		if err != nil {
+			log.Fatalf("Something went wrong while starting a proxy dialer for TCP conns.\n%v", err)
+		}
+		for _, site := range sitelist {
+			conn, err := proxyDialer.Dial("tcp4", site)
+			if err != nil {
+				log.Fatalf("%s unreachable, %v: ", site, err)
+			}
+			fmt.Println("[\u2713] TCP (IPv4) connection to", site)
+			log.WithFields(log.Fields{
+				"local":  conn.LocalAddr(),
+				"remote": conn.RemoteAddr(),
+			}).Infof("[\u2713] %s reachable via TCP (IPv4).\n", site)
+			conn.Close()
+		}
+	} else {
+		for _, site := range sitelist {
+			timeout := time.Duration(5 * time.Second)
+			conn, err := net.DialTimeout("tcp4", site, timeout)
+			if err != nil {
+				log.Fatalf("%s unreachable, %v: ", site, err)
+			}
+			fmt.Println("[\u2713] TCP (IPv4) connection to", site)
+			log.WithFields(log.Fields{
+				"local":  conn.LocalAddr(),
+				"remote": conn.RemoteAddr(),
+			}).Infof("[\u2713] %s reachable via TCP (IPv4).\n", site)
+			conn.Close()
+		}
+	}
+}

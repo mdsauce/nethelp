04c82121a1bebeefe7dc24fb4720676f98fc6804
mdsauce <max.dobeck@saucelabs.com>
2019-04-26 17:35:13 -0700

    Fixed build script.  Also added special Windows build for output.

---
+++ diagnostics/output_windows.go
@@ -0,0 +1,32 @@
+//+build windows
+
+package diagnostics
+
+import (
+	"fmt"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func respOutput(resp *http.Response, endpoint string) {
+	if resp.StatusCode == 200 {
+		fmt.Printf("[\u2713] %s is reachable %s\n", endpoint, resp.Status)
+		log.WithFields(log.Fields{
+			"status": resp.Status,
+			"resp":   resp,
+		}).Infof("[OK] %s reachable.\n", endpoint)
+	} else if resp.StatusCode == 401 {
+		fmt.Printf("[OK] %s is reachable but returned %s\n", endpoint, resp.Status)
+		log.WithFields(log.Fields{
+			"status": resp.Status,
+			"resp":   resp,
+		}).Infof("[OK] %s reachable but unauthenticated.\n", endpoint)
+	} else {
+		fmt.Printf("[ERROR] %s returned %s\n", endpoint, resp.Status)
+		log.WithFields(log.Fields{
+			"status": resp.Status,
+			"resp":   resp,
+		}).Infof("[ERROR] %s returned %s\n", endpoint, resp.Status)
+	}
+}

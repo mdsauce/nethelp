60cdded6b9a6b52c44fce422e3321764b901ad96
mdsauce <max.dobeck@saucelabs.com>
2019-06-05 17:56:49 -0700

    Started homebrew deploy process.

---
+++ nethelp.rb
@@ -0,0 +1,17 @@
+class Nethelp < Formula
+  desc "Find out why you can't reach Sauce Lab's services. (Real Device Cloud, Virtual Cloud, Sauce Connect and more)"
+  homepage "https://github.com/mdsauce/nethelp"
+  url ""
+  sha256 ""
+  depends_on "go" => :build
+  version "1"
+
+  def install
+    system "go", "build", "-o", bin/"nethelp", "."
+  end
+
+  test do
+    system "#{bin}/nethelp", --help, ">", "output.txt"
+    assert_predicate ./"output.txt", :exist?
+  end
+end

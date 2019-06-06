49abaf82db269779658e5705ba0b372681cbe7bc
mdsauce <max.dobeck@saucelabs.com>
2019-06-05 18:04:29 -0700

    v1.1 out

--- nethelp.rb
+++ nethelp.rb
@@ -1,7 +1,7 @@
 class Nethelp < Formula
   desc "Find out why you can't reach Sauce Lab's services. (Real Device Cloud, Virtual Cloud, Sauce Connect and more)"
   homepage "https://github.com/mdsauce/nethelp"
-  url "https://github.com/mdsauce/nethelp/archive/v1.tar.gz"
+  url "https://github.com/mdsauce/nethelp/archive/v1.1.tar.gz"
   sha256 ""
   depends_on "go" => :build
   version "1.1"

936060c653ae2163af8e0dda953be24bb95bbc90
mdsauce <max.dobeck@saucelabs.com>
2019-06-05 18:06:45 -0700

    Updated sha256 for homebrew

--- nethelp.rb
+++ nethelp.rb
@@ -2,7 +2,7 @@ class Nethelp < Formula
   desc "Find out why you can't reach Sauce Lab's services. (Real Device Cloud, Virtual Cloud, Sauce Connect and more)"
   homepage "https://github.com/mdsauce/nethelp"
   url "https://github.com/mdsauce/nethelp/archive/v1.1.tar.gz"
-  sha256 ""
+  sha256 "669e3f8f6f1c4b80e4fe045d1202c7236a576643ea8413d6934fd9fee8e90e6f"
   depends_on "go" => :build
   version "1.1"
 
